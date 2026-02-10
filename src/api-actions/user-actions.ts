812aca7e35596661b113026f85e0e78464e13459
Ashish Jangde <ashishjangde2003@gmail.com>
2026-02-10 20:56:22 +0530

    feat:fix interceptor and error handeling

--- src/api-actions/user-actions.ts
+++ src/api-actions/user-actions.ts
@@ -4,16 +4,16 @@ import { IUserUpdateSchema } from "@/schema/user.schema";
 export const UserActions = {
     GetCurrentUserAction: async (): Promise<IUser> => {
         const response = await axiosInstance.get<ApiResponse<IUser>>("/users/me");
-        return response.data.data!;
+        return response.data.data;
     },
 
     UpdateCurrentUserAction: async (data: IUserUpdateSchema): Promise<IUser> => {
         const response = await axiosInstance.put<ApiResponse<IUser>>("/users/me", data);
-        return response.data.data!;
+        return response.data.data;
     },
 
     DeleteCurrentUserAction: async (): Promise<IUniversalMessage> => {
         const response = await axiosInstance.delete<ApiResponse<IUniversalMessage>>("/users/me");
-        return response.data.data!;
+        return response.data.data;
     }
 }
