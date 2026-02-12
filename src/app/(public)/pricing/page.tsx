40c084581f4763c1ec8a40f467953f041926f05a
gautambamne <gautam2003b@gmail.com>
2026-02-12 14:55:27 +0530

    feat: Pricing page

---
+++ src/app/(public)/pricing/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import PricingModern from ".";
+import PricingComparator from "./pricing-comparator";
+const Example = () => (
+  <div className="h-full w-full">
+    <PricingModern/>
+    <PricingComparator/>
+  </div>
+);
+export default Example;
