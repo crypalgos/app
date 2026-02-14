015fd15e1ee6261a7ddb3133d6c58d97399a5469
gautambamne <gautam2003b@gmail.com>
2026-02-14 20:48:56 +0530

    feat: f and q design

--- src/app/(public)/pricing/page.tsx
+++ src/app/(public)/pricing/page.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import PricingModern from ".";
 import PricingComparator from "./pricing-comparator";
+import { AccordionFAQ } from "./accordion";
 
-const Page = () => (
-  <div className="h-full w-full">
-    <PricingModern/>
-    <PricingComparator/>
-  </div>
-);
+const Page = () => {
+  return (
+    <div className="h-full w-full">
+      <PricingModern />
+      <PricingComparator />
+      <AccordionFAQ />
+    </div>
+  );
+};
 export default Page;
