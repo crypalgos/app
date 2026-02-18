e43bf0585f0d08af657a705f7ee3c28c9d079042
Ashish Jangde <ashishjangde2003@gmail.com>
2026-02-18 20:21:48 +0530

    fix: remove .open-next build output from git

--- .open-next/server-functions/default/.next/server/chunks/ssr/_next-internal_server_app_(public)_terms-and-condition_page_actions_fd2c46fe.js
+++
@@ -1,3 +0,0 @@
-module.exports=[57323,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_%28public%29_terms-and-condition_page_actions_fd2c46fe.js.map
--- .open-next/server-functions/default/.next/server/chunks/ssr/_next-internal_server_app_(user)_dashboard_page_actions_41559d2f.js
+++
@@ -1,3 +0,0 @@
-module.exports=[24088,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_%28user%29_dashboard_page_actions_41559d2f.js.map
